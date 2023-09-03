9d61821525af120a0e5321dcdbf24a7b706efddf
mirza27 <ramadhanimirza10@gmail.com>
2023-09-03 21:39:57 +0700

    add backend folder

---
+++ server/app.js
@@ -0,0 +1,12 @@
+// server.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
