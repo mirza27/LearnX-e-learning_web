0cfc8b0373fcb2a83c75e19af83cfcf3755e8348
mirza27 <ramadhanimirza10@gmail.com>
2023-12-09 14:06:42 +0700

    update auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const secretKey = "hagsydgsdjkasdkbh7yiuJHBJGCD";
 
 // fungsi membuat token
-exports.generateToken = (payload) => {
+generateToken = (payload) => {
   const accessToken = jwt.sign(payload, secretKey, { expiresIn: "1m" });
 
   const refreshToken = jwt.sign(payload, secretKey, { expiresIn: "1h" });
@@ -12,7 +12,7 @@ exports.generateToken = (payload) => {
 };
 
 // Fungsi untuk memverifikasi token
-exports.verifyToken = (token) => {
+verifyToken = (token) => {
   try {
     const decoded = jwt.verify(token, secretKey);
     return decoded; // Token valid, kembalikan payload token
